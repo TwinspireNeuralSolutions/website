48c93e25939994883e488176995c7f4bd1394c9d
daryan-kf <daryankamalifar@gmail.com>
2025-06-01 14:30:31 +0200

    Add hero section with background image and Navbar component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+import { Navbar } from '@/components'
+
+import heroImage from '@/public/images/hero.jpeg'
+
 export default function Home() {
   return (
-    <div>
-      <main>Main</main>
+    <>
+      <Navbar />
+
+      <main style={{ minHeight: '4000px' }}>
+        {/* Section 1: Hero Section */}
+        <div
+          className="h-screen w-full bg-cover bg-center text-white"
+          style={{
+            backgroundImage: `url(${heroImage.src})`,
+            width: '100%',
+            height: '100vh',
+            filter: 'grayscale(100%)',
+          }}
+        ></div>
+        {/* Section 2: About Section */}
+        <div></div>
+      </main>
       <footer></footer>
-    </div>
-  );
+    </>
+  )
 }
