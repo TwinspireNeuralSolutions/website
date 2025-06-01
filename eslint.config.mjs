4c4755fa93e9dbc1dea2c177c1dac7bb1bbd6d19
daryan-kf <daryankamalifar@gmail.com>
2025-06-01 17:43:03 +0200

    Refactor ESLint configuration to include recommended rules and custom settings for React and TypeScript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+// eslint.config.js
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
@@ -8,9 +9,41 @@ const __dirname = dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+const config = [
+  ...compat.extends(
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier' // must be last to disable conflicting rules
+  ),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+  },
+  {
+    rules: {
+      // Custom rules
+      'react/react-in-jsx-scope': 'off', // Not needed in Next.js
+      'react/jsx-max-props-per-line': [1, { when: 'multiline' }],
+      'react/jsx-first-prop-new-line': ['error', 'multiline'],
+      'react/jsx-wrap-multilines': [
+        'error',
+        { declaration: 'parens-new-line' },
+      ],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+    },
+  },
 ]
 
-export default eslintConfig
+export default config
