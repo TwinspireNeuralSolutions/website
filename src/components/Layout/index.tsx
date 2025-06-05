80bcb4d3ec1de6b15b24c5f02a6f84903b0f4356
daryan-kf <daryankamalifar@gmail.com>
2025-06-05 18:21:50 +0200

    Add Layout component for consistent section structure, enabling flexible child rendering and customizable class names.

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,22 @@
+const Layout = ({
+  children,
+  id,
+  sectionClassName,
+  className,
+}: {
+  children: React.ReactNode
+  id?: string
+  sectionClassName?: string
+  className?: string
+}) => {
+  return (
+    <section
+      id={id}
+      className={`px-10 py-15 md:px-20 md:py-30 ${sectionClassName}`}
+    >
+      <div className={`mx-auto max-w-[1200px] ${className}`}>{children}</div>
+    </section>
+  )
+}
+
+export default Layout
