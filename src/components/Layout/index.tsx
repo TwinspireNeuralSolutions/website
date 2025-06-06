1c4ba49ed59dcbbaca9ac59136d04a0fad622332
daryan-kf <daryankamalifar@gmail.com>
2025-06-06 09:37:23 +0200

    Update Layout component to handle optional className and sectionClassName props, ensuring fallback to empty string for improved flexibility.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -12,9 +12,11 @@ const Layout = ({
   return (
     <section
       id={id}
-      className={`px-10 py-15 md:px-20 md:py-30 ${sectionClassName}`}
+      className={`px-10 py-15 md:px-20 md:py-30 ${sectionClassName ?? ''}`}
     >
-      <div className={`mx-auto max-w-[1400px] ${className}`}>{children}</div>
+      <div className={`mx-auto max-w-[1400px] ${className ?? ''}`}>
+        {children}
+      </div>
     </section>
   )
 }
