6930e734d234eada31ae04c7c6229c7dbc2a312f
daryan-kf <daryankamalifar@gmail.com>
2025-06-06 11:17:53 +0200

    Enhance Layout component to accept a ref prop, improving flexibility for parent component interactions and maintaining type safety with TypeScript.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,11 +3,19 @@ interface LayoutProps {
   id?: string
   sectionClassName?: string
   className?: string
+  ref?: React.RefObject<HTMLElement>
 }
 
-const Layout = ({ children, id, sectionClassName, className }: LayoutProps) => {
+const Layout = ({
+  children,
+  id,
+  sectionClassName,
+  className,
+  ref,
+}: LayoutProps) => {
   return (
     <section
+      ref={ref}
       id={id}
       className={`px-10 py-15 md:px-20 md:py-30 ${sectionClassName ?? ''}`}
     >
