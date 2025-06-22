e4a16caea31e737a30ef8ca249b6f7b2c30b27c0
daryan-kf <daryankamalifar@gmail.com>
2025-06-22 17:56:14 +0200

    Add definitions for ServicesContent component to support dynamic content rendering

---
+++ components/Modules/ServicesContent/definitions.ts
@@ -0,0 +1,28 @@
+const definitions = [
+  {
+    title: 'Pose Detection',
+    description: 'We use AI to detect the pose of a person in a video.',
+  },
+  {
+    title: 'Pose Detection',
+    description: 'We use AI to detect the pose of a person in a video.',
+  },
+  {
+    title: 'Pose Detection',
+    description: 'We use AI to detect the pose of a person in a video.',
+  },
+  {
+    title: 'Pose Detection',
+    description: 'We use AI to detect the pose of a person in a video.',
+  },
+  {
+    title: 'Pose Detection',
+    description: 'We use AI to detect the pose of a person in a video.',
+  },
+  {
+    title: 'Pose Detection',
+    description: 'We use AI to detect the pose of a person in a video.',
+  },
+]
+
+export default definitions
