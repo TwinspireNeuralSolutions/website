73ead1b842f411b4e52fb97b127abc97f8417f3b
daryan-kf <daryankamalifar@gmail.com>
2025-06-22 17:56:05 +0200

    Enhance Services component by integrating definitions for dynamic content rendering, adding H3 titles, and improving layout for active index display.

--- components/Modules/ServicesContent/index.tsx
+++ components/Modules/ServicesContent/index.tsx
@@ -3,7 +3,8 @@
 import { useState, useEffect, useRef } from 'react'
 import { useMediaQuery } from '@/hooks/useMediaQuery'
 import Layout from '@/components/Layout'
-import { H1 } from '@/components'
+import { H1, H3 } from '@/components'
+import definitions from './definitions'
 
 export const Services = ({ className }: { className?: string }) => {
   const { isMobile } = useMediaQuery()
@@ -29,18 +30,28 @@ export const Services = ({ className }: { className?: string }) => {
         <div
           className={`flex h-full w-full flex-col items-center gap-3 lg:flex-row ${className}`}
         >
-          {[0, 1, 2, 3, 4].map((idx) => {
+          {definitions.map(({ title, description }, idx) => {
             const sizingClasses =
               idx === activeIndex
                 ? 'w-full h-80 lg:h-full lg:w-full bg-white'
                 : 'flex-none w-full h-4 lg:h-full lg:w-6 bg-gray-800 hover:bg-gray-700 cursor-pointer'
+
+            const textClasses =
+              idx === activeIndex
+                ? 'rotate-0 mt-[-20px] text-left ml-[20px]'
+                : 'rotate-90 text-center mt-[20px]'
 
             return (
               <div
                 key={idx}
                 onClick={() => setActiveIndex(idx)}
                 className={`rounded-3xl transition-all duration-500 ease-in-out ${sizingClasses}`}
-              />
+              >
+                <H3 className={`whitespace-nowrap ${textClasses}`}>{title}</H3>
+                <div className="flex flex-col gap-2 text-black">
+                  {idx === activeIndex && <p>{description}</p>}
+                </div>
+              </div>
             )
           })}
         </div>
