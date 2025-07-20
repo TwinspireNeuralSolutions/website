b95ef88d81c97fa7d3cf3d15bafdb4ebcd0999e6
daryan-kf <daryankamalifar@gmail.com>
2025-07-20 12:41:31 +0200

    Enhance Button component styling by adding whitespace-nowrap class to prevent text wrapping, improving button appearance and usability.

--- components/Atoms/Button.tsx
+++ components/Atoms/Button.tsx
@@ -12,7 +12,7 @@ export const Button = ({
   return (
     <button
       onClick={onClick}
-      className={`text-md cursor-pointer rounded-full px-6 py-2 font-normal shadow-md transition ${
+      className={`text-md cursor-pointer rounded-full px-6 py-2 font-normal whitespace-nowrap shadow-md transition ${
         color === 'blue'
           ? 'bg-[#0802A3] text-white hover:bg-[#001060]'
           : 'bg-white text-black hover:bg-[#e0e0e0]'
