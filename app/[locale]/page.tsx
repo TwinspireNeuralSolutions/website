3bc70eac934d9854606f1bd1067e191d4a53ef43
PNikoui <57665675+PNikoui@users.noreply.github.com>
2026-09-13 15:45:24 +0200

    use concise public sections

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,13 +1,13 @@
 import { HeroStorySection } from '@/components/sections/Hero/HeroStory'
 import { PartnersSection } from '@/components/sections/Partners'
-import { ProblemSection } from '@/components/sections/Problem'
+import { PublicProblemSummarySection } from '@/components/sections/PublicProblemSummary'
 import { UseCasesSection } from '@/components/sections/UseCases'
 import { ScienceDataTrustSection } from '@/components/sections/ScienceDataTrust'
 import { ProductSection } from '@/components/sections/Product'
 import { BuiltForSection } from '@/components/sections/BuiltFor'
 import { TeamSection } from '@/components/sections/Team'
 import { DataEthicsSection } from '@/components/sections/DataEthics'
-import { FAQSection } from '@/components/sections/FAQ'
+import { PublicFAQSection } from '@/components/sections/PublicFAQ'
 import { ContactSection } from '@/components/sections/Contact'
 import { FooterSection } from '@/components/sections/Footer'
 import { Navbar } from '@/components/ui/navbar'
@@ -19,14 +19,14 @@ export default function Home() {
       <main>
         <HeroStorySection />
         <PartnersSection />
-        <ProblemSection />
+        <PublicProblemSummarySection />
         <UseCasesSection />
         <ScienceDataTrustSection />
         <ProductSection />
         <BuiltForSection />
         <TeamSection />
         <DataEthicsSection />
-        <FAQSection />
+        <PublicFAQSection />
         <ContactSection />
       </main>
       <FooterSection />
